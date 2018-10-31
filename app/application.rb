269914c95fad0dac6796feaffa7f45f7e32fa32f
lafesh <ginafedder@yahoo.com>
2018-10-31 15:40:38 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
 class Application
+    @@items = []
+
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
-        @@items = []
+        
 
         if req.path.match(/items/)
             item_name = req.path.split("/items/").last 
